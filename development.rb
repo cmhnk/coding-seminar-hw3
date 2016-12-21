509ac3aebc4925e9f6752c0272beb7ba7b8a0da1
cmhnk <corinne@automatedinsights.com>
2016-12-21 13:16:41 -0500

    Comment goal: to restrict add_employee method in the Department Class to only objects of Employee class

--- development.rb
+++ development.rb
@@ -1,6 +1,6 @@
 require './department.rb'
 
-corinne = Employee.new(name: 'Corinne Henk', email: 'corinne.m.henk@gmail.com', phone: 8602371500, salary: 60000)
+corinne = Employee.new(name: 'Corinne Henk', email: 'corinne.m.henk@gmail.com', phone: 8602371500, salary: 10000000)
 puts corinne.salary
 puts corinne.name
 
@@ -9,10 +9,14 @@ puts labs.name
 
 labs.add_employee(corinne)
 
-# p labs.list_employees
+p labs.list_employees
 
 corinne.write_review("Corinne laughs a lot.")
 puts corinne.get_review
 
 corinne.assign_department('Ai Labs')
 puts corinne.get_department
+
+# Issue: want to restrict who you can add to a department
+# Must be of 'Employee' class
+labs.add_employee('nick')
