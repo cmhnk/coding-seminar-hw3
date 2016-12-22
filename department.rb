ebfec94fcedff7731c17348381a5cde97dcb3d9c
cmhnk <corinne@automatedinsights.com>
2016-12-22 13:40:49 -0500

    tried to add a class variable to store all instances of the Department class, but did not work

--- department.rb
+++ department.rb
@@ -1,16 +1,23 @@
 require './employee.rb'
 #paul
 class Department
+  @@array = Array.new
   attr_reader :name, :employees
+
+  def self.all_instances
+    @@array
+  end
 
   def initialize(name:)
     @name = name
     @employees = []
+    @@array << self
   end
 
   def add_employee(e)
     @employees << e
   end
+  # e is a 'local attribute' I think
 
   def to_s
     to_s
--- development.rb
+++ development.rb
@@ -1,6 +1,9 @@
 require './department.rb'
 
 labs = Department.new(name: 'Ai Labs')
+analytics = Department.new(name: 'Analytics')
+qa = Department.new(name: 'QA')
+
 
 corinne = Employee.new(name: 'Corinne Henk', email: 'corinne.m.henk@gmail.com', phone: 8602371500, salary: 10000000)
 coop = Employee.new(name: 'Zack Cooper', email: 'zack@automated.insights.com', phone: 'XXX', salary: 200000)
@@ -9,13 +12,8 @@ coop = Employee.new(name: 'Zack Cooper', email: 'zack@automated.insights.com', p
 corinne.write_review('Corinne\'s pretty good at her job.')
 corinne.set_pass('Yes')
 
-# puts corinne.name
-# puts corinne.review
-puts corinne.pass
-
-
-
 labs.add_employee(corinne)
 labs.add_employee(coop)
 
-puts labs.employees.to_s
+# puts labs.employees.to_s
+puts Department.all_instances
